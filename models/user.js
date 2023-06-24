15017500dffad290aabee97c5dae07fedc75b1ac
GuravPratik <pratikgurav1010@gmail.com>
2023-06-24 20:26:57 +0530

    created a user schema and added a hook which convert plan password into hash, also add method to compare password with hash passwod

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxLength: [40, "Please should be more than 40 character"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter email in correct format"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: [8, "Password should be 8 or more character"],
+  },
+  photo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordMatch = async (userPassword) => {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
